c6b5c6f05549b5ced5fc25334a5beb3b5b331d92
Isaaac <nuchomiguel@hotmail.com>
2024-12-06 14:55:56 +0100

    t

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
                         <!-- Logo -->
                         <div class="col-xl-2 col-lg-2">
                             <div class="logo">
-                                <img src="assets/img/logoMIOP.jpg" alt="">
+                                <img src="assets/img/logoMIO.jpg" alt="">
                             </div>
                         </div>
                         <div class="col-xl-10 col-lg-10 col-md-10">
@@ -56,18 +56,32 @@
                             <div class="main-menu f-right d-none d-lg-block">
                                 <nav>
                                     <ul id="navigation">
-                                        <li><a href="index.html"> home</a></li>
-                                        <li><a href="about.html">About</a></li>
-                                        <li><a href="gallery.html">gallery</a></li>
-                                        <li><a href="booking.html">Guestbook</a></li>
-                                        <li><a href="blog.html">Blog</a>
+                                        <li><a href="index.html"> home</a></li>                                     
+                                        <li><a href="blog.html">Convites</a>
                                             <ul class="submenu">
-                                                <li><a href="blog.html">Blog</a></li>
-                                                <li><a href="single-blog.html">Blog Details</a></li>
-                                                <li><a href="elements.html">Element</a></li>
+                                                <li><a href="blog.html">Casamentos</a></li>
+                                                <li><a href="single-blog.html">Brindes</a></li>
+                                                <li><a href="elements.html">Lonas</a></li>
                                             </ul>
                                         </li>
-                                        <li><a href="contact.html">Contact Us</a></li>
+                                        <li><a href="blog.html">Uniformes</a>
+                                            <ul class="submenu">
+                                                <li><a href="blog.html">Polos</a></li>
+                                                <li><a href="single-blog.html">Coletes</a></li>
+                                                <li><a href="elements.html">T-shirts</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="blog.html">Eventos</a>
+                                            <ul class="submenu">
+                                                <li><a href="blog.html">Agendas</a></li>
+                                                <li><a href="single-blog.html">Álbuns</a></li>
+                                                <li><a href="single-blog.html"></a></li>
+                                                <li><a href="elements.html">Quadros</a></li>
+                                                <li><a href="elements.html">Portas Chaves</a></li>
+                                                <li><a href="elements.html">Lapizeiras</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="contact.html">Contactos</a></li>
                                     </ul>
                                 </nav>
                             </div>
