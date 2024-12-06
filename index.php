91c9c14209c044d757bb87354a01bf86ee9bfc68
Isaaac <nuchomiguel@hotmail.com>
2024-12-06 14:42:40 +0100

    Fase do Header

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/miop.png">
+    <title>Miop</title>´
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!--link rel="stylesheet" href="assets/css/owl.carousel.min.css"-->
+    <!--link rel="stylesheet" href="assets/css/flaticon.css"-->
+    <!--link rel="stylesheet" href="assets/css/slicknav.css"-->
+    <!--link rel="stylesheet" href="assets/css/animate.min.css"-->
+    <!--link rel="stylesheet" href="assets/css/magnific-popup.css"-->
+    <!--link rel="stylesheet" href="assets/css/fontawesome-all.min.css"-->
+    <!--link rel="stylesheet" href="assets/css/themify-icons.css"-->
+    <!--link rel="stylesheet" href="assets/css/slick.css"-->
+    <!--link rel="stylesheet" href="assets/css/nice-select.css"-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<style>
+    .top-bg {
+        background: #002d5b;
+    }
+
+    header Sticky Responsive .header-sticky ul li a {
+        padding: 10px 19px;
+    }
+
+    .header-sticky.sticky-bar.sticky .main-menu ul>li>a {
+        padding: 26px 20px;
+    }
+</style>
+
+<body>
+
+    <header>
+        <!-- Começo do Cabeçalho -->
+        <div class="header-area" style="border: 1px solid red;">
+            <div class="main-header header-sticky">
+
+                <div class="container">
+                    <div class="row align-items-center">
+                        <!-- Logo -->
+                        <div class="col-xl-2 col-lg-2">
+                            <div class="logo">
+                                <img src="assets/img/logoMIOP.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-xl-10 col-lg-10 col-md-10">
+                            <!-- Main-menu -->
+                            <div class="main-menu f-right d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a href="index.html"> home</a></li>
+                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="gallery.html">gallery</a></li>
+                                        <li><a href="booking.html">Guestbook</a></li>
+                                        <li><a href="blog.html">Blog</a>
+                                            <ul class="submenu">
+                                                <li><a href="blog.html">Blog</a></li>
+                                                <li><a href="single-blog.html">Blog Details</a></li>
+                                                <li><a href="elements.html">Element</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="contact.html">Contact Us</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Fim do cabeçalho -->
+
+    </header>
+</body>
+
+</html>
